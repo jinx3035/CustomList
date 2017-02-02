8ff7580671e9a464d760f5c40ea2d570c8cae9ba
jinx3035 <joebob1209@yahoo.com>
2017-02-02 13:48:29 -0600

    created add and remove tests

---
+++ CustomList/CustomListClass.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomList
+{
+    public class CustomListClass
+    {
+        public int Count = 10;
+
+        public int Add(int item)
+        {
+            return Count;
+        }
+
+        public int Remove(int item)
+        {
+            return Count;
+        }
+
+    }
+}
---
+++ CustomListTest/UnitTest1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CustomList;
+
+namespace CustomListTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void AddToList()
+            {
+            //Arrange
+            CustomListClass list = new CustomListClass();
+            int item = 1;
+            int result;
+            //Act
+            result = list.Add(item);
+            //Assert
+            Assert.AreEqual(result, list.Count);
+        }
+
+        [TestMethod]
+        public void RemoveFromList()
+        {
+            //Arrange
+            CustomListClass list = new CustomListClass();
+            int item = 1;
+            int result;
+            //Act
+            result = list.Remove(item);
+            //Assert
+            Assert.AreEqual(result, list.Count);
+        }
+    }
+}
